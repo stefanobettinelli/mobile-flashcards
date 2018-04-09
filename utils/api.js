de21957c8980fa27af7ab5745ec356854bdf3c31
Stefano Bettinelli <stefano.bettinelli85@gmail.com>
2018-04-09 19:09:35 +0200

    All API functions implemented

--- utils/api.js
+++ utils/api.js
@@ -5,20 +5,23 @@ const DECKS_STORAGE_KEY = "MobileFlashCards:decks";
 // return all decks with title, questions and answers
 export function getDecks() {
   return AsyncStorage.getItem(DECKS_STORAGE_KEY).then(
-    decks =>
-      decks === null ? "there are no decks at the moment" : JSON.parse(decks)
+    decks => (decks === null ? {} : JSON.parse(decks))
   );
 }
 
 // takes in a single ID and returns the deck associated with that ID (let's assume is the lower cased title)
-export function getDeck(id) {}
+export function getDeck(title) {
+  return AsyncStorage.getItem(DECKS_STORAGE_KEY)
+    .then(decks => (decks === null ? {} : JSON.parse(decks)))
+    .then(decks => decks[title.toLowerCase()]);
+}
 
 // takes a title argument and add it to the decks
 export function saveDeckTitle(title) {
   const newDeck = {
     [title.toLowerCase()]: {
       title,
-      questionsAndAnswers: []
+      cards: []
     }
   };
 
@@ -26,7 +29,19 @@ export function saveDeckTitle(title) {
 }
 
 // add the 'card' to the deck with title 'title'
-export function addCardToDeck(title, card) {}
+export function addCardToDeck(title, card) {
+  return getDeck(title)
+    .then(deck => ({
+      ...deck,
+      cards: [...deck.cards, card]
+    }))
+    .then(modifiedDeck =>
+      AsyncStorage.mergeItem(
+        DECKS_STORAGE_KEY,
+        JSON.stringify({ [title.toLowerCase()]: modifiedDeck })
+      )
+    );
+}
 
 export function removeDecks() {
   AsyncStorage.removeItem(DECKS_STORAGE_KEY, () =>
